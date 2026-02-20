5c431e71c78a757bc8a1368309dc4fd3a5079bd6
Qutub khan <kutubmerchant598@gmail.com>
2026-02-20 15:21:07 +0530

    bug fixes

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Clients from '../components/home/Clients';
 import ProjectsCards from '../components/projects/ProjectsCards';
 const ProjectsPage = () => {
     return (
